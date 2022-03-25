e86c4f38eb37e60656829b9dc1c3a9498a088563
Tomasz Pawliczak <pawliczak.tomasz@gmail.com>
2022-03-25 12:22:01 +0100

    Adding a note

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -4,6 +4,8 @@ import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
 import NotFound from "./containers/NotFound";
 import Signup from "./containers/Signup";
+import NewNote from "./containers/NewNote";
+import Notes from "./containers/Notes";
 
 export default function Routes() {
     return (
@@ -17,6 +19,13 @@ export default function Routes() {
             <Route exact path="/signup">
                 <Signup />
             </Route>
+            <Route exact path="/notes/new">
+            <NewNote />
+            </Route>
+            <Route exact path="/notes/:id">
+            <Notes />
+            </Route>
+
             {/* Finally, catch all unmatched routes */}
             <Route>
                 <NotFound />
